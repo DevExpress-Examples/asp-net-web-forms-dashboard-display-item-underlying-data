b7a3164932e1694228b0b1f5dca78834f74a3ea2
DevExpressExampleBot <fakeEmail>
2022-06-21 19:50:03 +0400

    Source auto update [skip ci]

--- VB/ASPxDashboard_RequestUnderlyingData/Scripts/UnderlyingData.js
+++ VB/ASPxDashboard_RequestUnderlyingData/Scripts/UnderlyingData.js
@@ -1,21 +1,32 @@
+function onBeforeRender(sender) {
+    var dashboardControl = sender.GetDashboardControl();
+
+    if (dashboardControl)
+        dashboardControl.on('dashboardEndUpdate', loadUnderlyingData);
+}
+
 function loadUnderlyingData() {
+
+    webDashboard = clientDashboardControl.GetDashboardControl();
+    var viewerApiExtension = webDashboard.findExtension('viewer-api');
+
     // Gets client data displayed in the Card dashboard item.
-    var clientData = webDashboard.GetItemData('cardDashboardItem1');
+    var clientData = viewerApiExtension.getItemData('cardDashboardItem1');
 
     // Creates an array of data members used to obtain underlying data.
-    var availableDataMembers = clientData.GetDataMembers();
+    var availableDataMembers = clientData.getDataMembers();
     var specificDataMembers = availableDataMembers.filter(function (dataMember) {
         return ['Sales Person', 'Extended Price', 'OrderDate'].indexOf(dataMember) !== -1;
     });
 
     // Gets an axis point corresponding to a specific employee.
-    var allSalesPersons = clientData.GetAxis(DashboardDataAxisNames.DefaultAxis).GetPoints();
+    var allSalesPersons = clientData.getAxis(DashboardDataAxisNames.DefaultAxis).getPoints();
     var specificSalesPerson = allSalesPersons.find(function (person) {
         return person.GetValue() === "Andrew Fuller";
     });
 
     // Gets an axis point corresponding to a specific month.
-    var allDates = clientData.GetAxis(DashboardDataAxisNames.SparklineAxis).GetPoints();
+    var allDates = clientData.getAxis(DashboardDataAxisNames.SparklineAxis).getPoints();
     var specificDate = allDates.find(function (date) {
         return date.GetValue().toDateString() === (new Date(2016, 4, 1)).toDateString();
     });
@@ -29,12 +40,12 @@ function loadUnderlyingData() {
     var underlyingData = [];
     
     // Calls the RequestUnderlyingData method for the Card dashboard item with the specified parameters.
-    webDashboard.RequestUnderlyingData('cardDashboardItem1', requestParameters, function (data) {
-        dataMembers = data.GetDataMembers();
-        for (var i = 0; i < data.GetRowCount() ; i++) {
+    viewerApiExtension.requestUnderlyingData('cardDashboardItem1', requestParameters, function (data) {
+        dataMembers = data.getDataMembers();
+        for (var i = 0; i < data.getRowCount() ; i++) {
             var dataTableRow = {};
             $.each(dataMembers, function (_, dataMember) {
-                dataTableRow[dataMember] = data.GetRowValue(i, dataMember);
+                dataTableRow[dataMember] = data.getRowValue(i, dataMember);
             });
             underlyingData.push(dataTableRow);
         }
